ad355e9e2679fb85fea3d191a66e9e36f38b8013
Mikki-dev-git <73258978+Mikki-dev-git@users.noreply.github.com>
2021-03-30 02:06:25 +0200

    commit

---
+++ ex03/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React, { Component } from 'react';
+import Plant from './Plant';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Plant />
+      </div>
+    );
+  }
+}
+
+export default App;
